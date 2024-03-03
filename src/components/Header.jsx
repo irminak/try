e6e34aaa2c9f94e937f54106c753119096cfb360
Irmina Kalinowska <irminakalinowska0@gmail.com>
2024-03-03 22:29:14 +0100

    header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import Button from './Button';
+
+import header from '../assets/header.png';
+import avo from '../assets/avo.png';
+import tomato from '../assets/tomato.png';
+
+const Header = () => {
+    return (
+        <header>
+            <img
+                className='avo'
+                src={avo}
+                alt=''
+            />
+            <img
+                className='tomato'
+                src={tomato}
+                alt=''
+            />
+
+            <h2 className='first'>just</h2>
+            <h1 className='second'>
+                <span>Try</span>.
+            </h1>
+            <img
+                className='fifth'
+                src={header}
+                alt='Food bowl'
+            />
+            <div className='third'>
+                <h3>
+                    Transform Your Health with Our Tried and Tested Diet
+                    Programs.
+                </h3>
+                <div>
+                    <Button text='View Meal Plans' />
+                </div>
+            </div>
+            <h3 className='fourth'>
+                Discover Tailored Diet Plans to Suit Every Lifestyle, Expertly
+                Crafted for Your Success. Your healthly lifestyle coach.
+            </h3>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className='logo'>
+                <img
+                    src=''
+                    alt=''
+                />
+            </div>
+            <div className='navigation'>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Offer</li>
+                        <li>Recipes</li>
+                        <li>Mobile App</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+            <div className='cart'>
+                <a></a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
